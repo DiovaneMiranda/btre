4caf50396c2ba67bc8b3d140f5ed56fc5a282928
diovane <diovane.miranda@hotmail.com>
2018-11-11 15:16:58 -0200

    Listings url and templates updates

--- btre/urls.py
+++ btre/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
-    path('listing/', include('listings.urls')),
+    path('listings/', include('listings.urls')),
     path('realtors/', include('realtors.urls')),
     path('admin/', admin.site.urls),
 ]
